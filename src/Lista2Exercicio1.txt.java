c3b22f0deb332a2fe725838d2b33cec983e2ef94
13RodriguesBruna <107412241+13RodriguesBruna@users.noreply.github.com>
2022-09-29 13:31:02 -0300

    Update Lista2Exercicio1.txt.java

--- src/Lista2Exercicio1.txt.java
+++ src/Lista2Exercicio1.txt.java
@@ -20,22 +20,24 @@ public class Main {
         contaminha.dono= "Bruna";
         contaminha.saldo=100000.00;
         contaminha.numero=13;
+        contaminha.limite=1000.00;
 
         contaIron.dono= "Iron";
         contaIron.saldo = 10000.0;
         contaIron.numero=3;
+        contaIron.limite=15000.00
 
         System.out.println("Conta Bruna informações:");
 
          System.out.println("Nome conta:"+contaminha.dono);
          System.out.println("Saldo conta:"+contaminha.saldo);
          System.out.println("Número conta:"+contaminha.numero);
+         System.out.println("Limite conta"+contaminha.limite);  
 
         System.out.println("\nConta Iron informações:");
         System.out.println("Nome conta:"+contaIron.dono);
         System.out.println("Saldo conta:"+contaIron.saldo);
-        
-        System.out.println("Saldo conta:"+contaIron.saldo);
-        System.out.println("Numero conta:" +contaIron.numero);
+        System.out.println("Limite conta"+contaIron.limite);  
 
+        
         
